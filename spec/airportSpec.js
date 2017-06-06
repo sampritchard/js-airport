15ee405d200bbdf8faac03bdcc2a496ac277cfe5
wemmm <jenny.wem@gmail.com>
2017-06-06 15:38:17 +0100

    Feature and unit tests for stormy conditions preventing takeoff, both passed

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -19,4 +19,20 @@ describe('Airport', function(){
     airport.clearForTakeOff(plane);
     expect(airport.planes()).toEqual([]);
   });
+  it('can check for stormy conditions', function(){
+    expect(airport.isStormy()).toBeFalsy();
+  });
+});
+
+describe('Under Stormy Conditions', function(){
+  var airport;
+  var plane;
+  beforeEach(function(){
+    airport = new Airport();
+    plane = new Plane();
+  });
+  it('does not clear planes for takeoff', function(){
+    spyOn(airport,'isStormy').and.returnValue(true);
+    expect(function(){ airport.clearForTakeOff(plane); }).toThrowError('cannot takeoff during storm');
+  });
 });
