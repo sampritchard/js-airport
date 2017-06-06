0087163c7d2d1b5710816c4d391e4547550fa187
Sam Pritchard <sampritchard@msn.com>
2017-06-06 13:51:57 +0100

    fixed planeSpec.js by adding clearForTakeOff for spy

--- src/plane.js
+++ src/plane.js
@@ -3,6 +3,7 @@
 function Plane(){}
 Plane.prototype.land = function(airport){
   airport.clearForLanding(this);
+  this._location = airport;
 };
 Plane.prototype.takeoff = function(){
   this._location.clearForTakeOff();
